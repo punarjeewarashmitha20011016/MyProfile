bb7dd9771118e831516d3a2f8b48795dc81e31d9
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-01-28 18:52:07 +0530

    done full responsive of my profile

---
+++ assets/JS/script.js
@@ -0,0 +1,5 @@
+var formContentHr = document.getElementById("formContentHr");
+var formContent = document.querySelector(".contact .content");
+
+var heightOfContent = formContent.offsetHeight;
+formContentHr.style.width=heightOfContent+"px";
